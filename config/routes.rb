2829920080b05a79fec4d9fe96a694405b0e36a4
manojvasava <manojvasava5uk@gmail.com>
2023-11-17 17:25:30 +0000

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "lists#index"
+  root "lists#index"
   # get "lists/new", to: "lists#new", as: :new
   # get "lists/:id", to: "lists#show", as: :show
   # # Show List by id of Tasks
